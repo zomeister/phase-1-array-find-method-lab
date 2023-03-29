12348bbc3d105a2e90eaa0be709a0c1dd874e532
Zoe Guidry <oyezoey@gmail.com>
2023-03-29 09:03:42 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function isWinner(team) {
+    return team.result === "W"
+}
+
+
+function superbowlWin(arr) {
+    const winner = arr.find(isWinner)
+    if (winner === undefined) {return undefined}
+    else { return winner.year }
+
+}
